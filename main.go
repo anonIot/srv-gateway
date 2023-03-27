b2eef92a437ee97741d428b69e02830ef29d8a14
Anon D <acework2u@gmail.com>
2023-03-27 14:19:40 +0700

    init main

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("SRV Mini API")
+		return c.SendString("SRV Mini Gateway API Service")
 	})
 
 	app.Listen(":3033")
